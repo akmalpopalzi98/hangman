9515df858e498c666a486292b07a69f56785d667
akmalpopalzi98 <akmalpopalzi.eng@gmail.com>
2023-01-12 18:42:05 +0000

    adding task 2 code

--- milestone_3.py
+++ milestone_3.py
@@ -1,6 +1,20 @@
+#%%
 while True:
     guess = input('guess the letter')
     if len(guess) == 1 and guess.isalpha():
         break
     else:
         print('invalid letter. Please enter a valid letter')
+
+#%%
+guess
+
+#%%
+word = 'secret'
+
+# %%
+if guess in word:
+    print(f'Good guess! {guess} is in the word')
+else:
+    print(f'Sorry, {guess} is not in the word')
+# %%
