76b69902680648d7f639dfbf52f9b06398527466
akmalpopalzi98 <akmalpopalzi.eng@gmail.com>
2023-01-12 15:41:19 +0000

    fixing error

--- milestone_2.py
+++ milestone_2.py
@@ -16,7 +16,7 @@ print(word)
 guess = input('enter a single letter')
 
 # %%
-if len(guess) == 1 and isinstance(guess,str):
+if len(guess) == 1 and guess.isalpha():
     print('good guess')
 else:
     print('Oops! That is not valid')
