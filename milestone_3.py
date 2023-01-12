7595c5567d9ae5d9558a60a1bc525d62a0c18649
akmalpopalzi98 <akmalpopalzi.eng@gmail.com>
2023-01-12 21:18:00 +0000

    adding functions

--- milestone_3.py
+++ milestone_3.py
@@ -18,3 +18,18 @@ if guess in word:
 else:
     print(f'Sorry, {guess} is not in the word')
 # %%
+def check_guess(letter):
+    guessed_letter = letter.lower()
+    if guessed_letter in word:
+        print(f'Good guess! {guessed_letter} is in the word')
+    else:
+        print(f'Sorry, {guessed_letter} is not in the word')
+
+#%%
+def ask_for_input():
+    while True:
+        guess = input('guess the letter')
+        if len(guess) == 1 and guess.isalpha():
+            break
+        else:
+            print('invalid letter. Please enter a valid letter')
