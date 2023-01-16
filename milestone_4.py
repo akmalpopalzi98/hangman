d53fa2ff0f4416cc9ce7571154c0bb58e03bcfcd
akmalpopalzi98 <105284469+akmalpopalzi98@users.noreply.github.com>
2023-01-16 10:48:02 +0000

    reverting changes

--- milestone_4.py
+++ milestone_4.py
@@ -5,7 +5,7 @@ class Hangman():
     def __init__(self,word_list,num_lives = 5):
         self.word_list = word_list
         self.num_lives = num_lives
-        self.word = random.choice(self.word_list).lower()
+        self.word = random.choice(self.word_list)
         self.word_guessed = list('_'*len(self.word))
         self.num_letters = len(set(self.word))
         self.list_of_guesses = []
